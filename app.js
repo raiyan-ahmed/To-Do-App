d8e5a6ae3bb77bb1d99b3197acb0f20ddfe559b9
raiyan-ahmed <rfraiyan123@gmail.com>
2022-09-14 03:04:11 +0600

    select dom

---
+++ app.js
@@ -0,0 +1,5 @@
+//Select DOM
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
