02c50d2b32a342531d92b7701b9eda41634a10e1
raiyan-ahmed <rfraiyan123@gmail.com>
2022-09-14 17:55:17 +0600

    add function

--- app.js
+++ app.js
@@ -9,3 +9,36 @@ document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteTodo);
 filterOption.addEventListener("click", filterTodo);
+
+//Add Function
+//Functions
+
+function addTodo(e) {
+    //Prevent natural behaviour
+    e.preventDefault();
+    //Create todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create list
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    //Save to local - do this last
+    //Save to local
+    saveLocalTodos(todoInput.value);
+    //
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    todoInput.value = "";
+    //Create Completed Button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Create trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //attach final Todo
+    todoList.appendChild(todoDiv);
+}
